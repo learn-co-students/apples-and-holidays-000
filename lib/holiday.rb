2e0744c44273b41defcbdfe8968342e7d8e4a346
danielglatstein <danielglatstein@gmail.com>
2015-06-26 11:07:50 -0400

    update bbq method

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,4 +72,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_array = [ ]
+  holiday_hash.each do |season, value|
+    value.each do |holiday, supply|
+      if supply.include?('BBQ')
+        bbq_array << holiday
+      end
+    end
+  end
+  bbq_array
 end
